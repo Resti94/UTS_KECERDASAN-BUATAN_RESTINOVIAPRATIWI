e88fa95f498f82827f764c959a351defd10ca4d9
RESTI <102235063+Resti94@users.noreply.github.com>
2022-04-22 15:26:24 +0700

    Add files via upload

---
+++ UTS_191011400824_RESTINOVIAPRATIWI.py
@@ -0,0 +1,32 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+# if nama=="Resti" : print("tetap semangat !!!!")
+# print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+
+# if nama=="Resti":
+# 	print("tetap semangat!")
+# 	print("kamu juga keren banget!")
+# print(f"Terima kasih {nama}")
+
+# 3. Else Statement
+
+if nama==" Esa ":
+	print("hai Esa !!!")
+else:
+	print(" Esa tak semangat! :(")
+
+print("akhir dari program")
+
---
+++ main.py
@@ -0,0 +1,9 @@
+print("RESTI NOVIA PRATIWI_191011400824_06TPLM005")
+nilai = 85
+
+print('Nilai anda adalah:', nilai, '\n')
+
+if nilai >= 70:
+  print('Selamat, anda lulus!')
+else:
+  print('Maaf, anda tidak lulus.')
